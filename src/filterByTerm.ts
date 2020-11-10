acc826ca423e7cc9842dc0120dcc5c075d771678
vaughan189 <vaughan188@gmail.com>
2020-11-10 16:29:35 +0530

    extending interfaces

--- src/filterByTerm.ts
+++ src/filterByTerm.ts
@@ -2,23 +2,34 @@ interface Link {
     description?: string;
     id?: number;
     url: string;
-  }
-  
-  function filterByTerm(input: Array<Link>, searchTerm: string) {
+}
+interface TranslatedLink extends Link {
+    language: string;
+}
+
+const link1: TranslatedLink = {
+    description:
+        "TypeScript tutorial for beginners is a tutorial for all the JavaScript developers ...",
+    id: 1,
+    url: "www.valentinog.com/typescript/",
+    language: "en"
+};
+
+function filterByTerm(input: Array<Link>, searchTerm: string) {
     if (!searchTerm) throw Error("searchTerm cannot be empty");
     if (!input.length) throw Error("input cannot be empty");
     const regex = new RegExp(searchTerm, "i");
-    return input.filter(function(arrayElement) {
-      return arrayElement.url.match(regex);
+    return input.filter(function (arrayElement) {
+        return arrayElement.url.match(regex);
     });
-  }
-  
-  const obj1: Link = { url: "string1" };
-  const obj2: Link = { url: "string2" };
-  const obj3: Link = { url: "string3" };
-  
-  const arrOfLinks: Array<Link> = [obj1, obj2, obj3];
-  
-  const term: string = "java";
-  
-  filterByTerm(arrOfLinks, term);
+}
+
+const obj1: Link = { url: "string1" };
+const obj2: Link = { url: "string2" };
+const obj3: Link = { url: "string3" };
+
+const arrOfLinks: Array<Link> = [obj1, obj2, obj3];
+
+const term: string = "java";
+
+filterByTerm(arrOfLinks, term);
