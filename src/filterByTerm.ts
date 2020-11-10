001277cd29b39fdf0a71065cf892baa1b438abd2
vaughan189 <vaughan188@gmail.com>
2020-11-10 15:34:42 +0530

    TypeScript objects and interfaces

---
+++ src/filterByTerm.ts
@@ -0,0 +1,19 @@
+interface Link {
+    description?: string;
+    id?: number;
+    url: string;
+}
+
+function filterByTerm(input: Array<Link>, searchTerm: string) {
+    if (!searchTerm) throw Error("searchTerm cannot be empty");
+    if (!input.length) throw Error("input cannot be empty");
+    const regex = new RegExp(searchTerm, "i");
+    return input.filter(function (arrayElement) {
+        return arrayElement.url.match(regex);
+    });
+}
+
+filterByTerm(
+    [{ url: "string1" }, { url: "string2" }, { url: "string3" }],
+    "java"
+);
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,6 @@
 var aBoolean = false;
 console.log(typeof aBoolean); // "boolean"
 
+// Type error - Type 'string' is not assignable to type 'boolean'.
 aBoolean = "Tom";
 console.log(typeof aBoolean); // "string"
