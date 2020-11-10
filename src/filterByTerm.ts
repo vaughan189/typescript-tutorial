0859a300638c21c5119d4ac887afdac2c566020a
vaughan189 <vaughan188@gmail.com>
2020-11-10 16:17:51 +0530

    typing variables

--- src/filterByTerm.ts
+++ src/filterByTerm.ts
@@ -2,18 +2,23 @@ interface Link {
     description?: string;
     id?: number;
     url: string;
-}
-
-function filterByTerm(input: Array<Link>, searchTerm: string) {
+  }
+  
+  function filterByTerm(input: Array<Link>, searchTerm: string) {
     if (!searchTerm) throw Error("searchTerm cannot be empty");
     if (!input.length) throw Error("input cannot be empty");
     const regex = new RegExp(searchTerm, "i");
-    return input.filter(function (arrayElement) {
-        return arrayElement.url.match(regex);
+    return input.filter(function(arrayElement) {
+      return arrayElement.url.match(regex);
     });
-}
-
-filterByTerm(
-    [{ url: "string1" }, { url: "string2" }, { url: "string3" }],
-    "java"
-);
+  }
+  
+  const obj1: Link = { url: "string1" };
+  const obj2: Link = { url: "string2" };
+  const obj3: Link = { url: "string3" };
+  
+  const arrOfLinks: Array<Link> = [obj1, obj2, obj3];
+  
+  const term: string = "java";
+  
+  filterByTerm(arrOfLinks, term);
